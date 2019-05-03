a6b295fa72b7aa67418e7890d147b48bcd792000
JoeySuc89 <jsucameli1@gmail.com>
2019-05-02 21:23:19 -0400

    wastd time added an item to the table

--- great_bayDB.sql
+++ great_bayDB.sql
@@ -8,7 +8,9 @@ CREATE TABLE items (
 	id INTEGER AUTO_INCREMENT NOT NULL,
     productName VARCHAR(255) NOT NULL,
     quantity INTEGER,
-    price FLOAT(4, 2),
+    price FLOAT(7,2),
     PRIMARY KEY(id)
 );
 
+INSERT INTO items(productName, quantity, price)
+VALUES ("Lizzie McGuire Sleeping Bag", 1, 250.00);
